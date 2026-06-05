93fb8bcae7a56bc1f8bf7ce7e0bfd828a876430d
BUVANA4022 <240346.EC@rmkec.ac.in>
2026-06-05 19:29:03 +0530

    Create check_alphanumeric.cpp
    Check for the presence of all characters in the range a-z, A-Z  and 0-9

---
+++ check_alphanumeric.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cin>>a;
+    int flag1=0,flag2=0,flag3=0;
+    for(int i=0;i<a.length();i++){
+        flag1 = flag1 | (1<<(a[i]-'a'));
+        flag2 = flag2 | (1<<(a[i]-'A'));
+        if(a[i]>='0' && a[i]<='9')
+       flag3 = flag3 | (1<<(a[i]-'0'));
+    }
+    cout<<(((flag2 == (1<<26)-1)  && (flag2 == (1<<26)-1) &&(flag3 ==(1<<10)-1))?"yes":"no");
+
+
+
+    return 0;
+}
